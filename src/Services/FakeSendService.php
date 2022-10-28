596b84cca5e6b6e6c0ae01a282d30bc144e0f6e5
Wilfried <wilfried@web-id.fr>
2022-10-28 02:08:47 +0200

    🐛 Fix errors for CI

--- src/Services/FakeSendService.php
+++ src/Services/FakeSendService.php
@@ -12,6 +12,7 @@ class FakeSendService extends AbstractSendService
 {
     private static ?Message $lastSentMessage = null;
 
+    /** @var array<string,mixed> */
     private static array $lastSentMessageConfig = [];
 
     public static function reset(): void
@@ -29,7 +30,7 @@ class FakeSendService extends AbstractSendService
     {
         PHPUnit::assertNotNull(self::$lastSentMessage);
         PHPUnit::assertEquals(
-            self::$lastSentMessage->getData(),
+            self::$lastSentMessage?->getData(),
             $message->getData()
         );
     }
